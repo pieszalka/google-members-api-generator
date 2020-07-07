b264b67be0f76c3208ac07d69758ee1d5ceb806a
Maciej Pieszala <maciej.pieszala@allegro.pl>
2020-07-07 23:51:29 +0200

    init

---
+++ src/main/scala/com/fourthwall/googlemembersapi/GoogleMembersApiApplication.scala
@@ -0,0 +1,11 @@
+package com.fourthwall.googlemembersapi;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GoogleMembersApiApplication
+
+object GoogleMembersApiApplication extends App {
+	SpringApplication.run(classOf[GoogleMembersApiApplication], args: _*)
+}
---
+++ src/test/scala/com/fourthwall/googlemembersapi/GoogleMembersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourthwall.googlemembersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMembersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
